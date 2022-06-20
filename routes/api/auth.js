8663a9c8324ceb2c281715307a47b12e9ea05d47
Dmitriy Vasylchuk <dmvs.9898@gmail.com>
2022-06-20 13:56:36 +0300

    add files

---
+++ routes/api/auth.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const { validation, ctrlWrapper } = require("../../middlewares");
+const { auth: ctrl } = require("../../controllers");
+const { joiRegisterSchema } = require("../../models/user");
+
+router.post(
+  "/register",
+  validation(joiRegisterSchema),
+  ctrlWrapper(ctrl.register)
+);
+
+module.exports = router;
